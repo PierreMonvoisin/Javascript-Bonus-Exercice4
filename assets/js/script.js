1956cfb74f0271b5f6fd25df47d3526c8f78a316
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-10-30 21:58:32 +0100

    Exercice en cours, avancé sur le prénom et nom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-document.getElementById('firstName').onkeyup = formCheck;
-document.getElementById('lastName').onkeyup = formCheck;
+document.getElementById('lastName').addEventListener('input', function() {formCheck(this.value)} ); // Récupère la value de l'input
+document.getElementById('firstName').addEventListener('input', function() {formCheck(this.value)} );
 document.getElementById('mail').onkeyup = formCheck;
 document.getElementById('phoneNumber').onkeyup = formCheck;
 
-function formCheck(){
-  let firstName = document.getElementById('firstName').value, lastName = document.getElementById('lastName').value, mail = document.getElementById('mail').value, phoneNumber = document.getElementById('phoneNumber').value;
-  // FirstName Check
-  this.style.border = "2px solid red";
-  // NumberCheck
+document.getElementById('button').disabled = 'true'; // Devient 'true' quand tous les champs sont validés
+
+function formCheck(variable){
+  var firstName = document.getElementById('firstName').value, lastName = document.getElementById('lastName').value, mail = document.getElementById('mail').value, phoneNumber = document.getElementById('phoneNumber').value;
+  const letters = /^[A-Za-z]+$/;
+  // firstName Check
+  let char = (variable.charAt(variable.length - 1)); // Isole la dernière lettre de la chaine
+  ((char.match(letters)) ? char : char.replace('')); // Essaie de remplacer l'input par '' si char n'est pas une lettre
 };
